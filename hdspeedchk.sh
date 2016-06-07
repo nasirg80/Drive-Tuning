03a176fa570b7f47831dc95e267b8f3bb3dd032a
Nasir Grewal <nasirg80@users.noreply.github.com>
2016-06-07 00:42:30 -0400

    Update hdspeedchk.sh
    Work in progress....

--- hdspeedchk.sh
+++ hdspeedchk.sh
@@ -8,24 +8,33 @@
 #           Check speed of USB/SD/SSD etc.       #
 #************************************************#
 
-E_BADDIR=85                       # No such directory.
-projectdir=/home/bozo/projects    # Directory to clean up.
+E_BADDEV=85                       # No such storage device
 
 # --------------------------------------------------------- #
-# cleanup_pfiles ()                                         #
-# Removes all files in designated directory.                #
-# Parameter: $target_directory                              #
-# Returns: 0 on success, $E_BADDIR if something went wrong. #
+# perform_timing()                                          #
+# Gets average read speed of all storage devices on pc      #
+# Returns: 0 on success, $E_BADDEV if something went wrong. #
 # --------------------------------------------------------- #
 
 $ echo /dev/sd{a..h}
 /dev/sda /dev/sdb /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg /dev/sdh
 
+if [ ! -d "$1" ]  # Test if target device exists.
+  then
+    echo "$1 is not a storage device."
+    return $E_BADDEV
+  fi
+
 perform_timing() {
     for i in {1..6}; do hdparm -t "$1"; done |
         awk '/seconds/ { total += $11; count++ } END { print (total / count) }'
+        return 0   # Success.
 }
 
 for drive in /dev/sd{a..h}; do
     printf '%s: %s\n' "$drive" "$(perform_timing "$drive")"
 done
+
+return 0   # Success.
+
+exit $?
